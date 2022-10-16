ac23797c5710f8c01152c13a65a084e3d47467dd
adithya-6337 <veldandiadithya337@gmail.com>
2022-10-16 19:10:38 +0530

    Added : Float find Maximum

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -2,7 +2,10 @@ package com.bridgelabz;
 
 public class Main {
     public static void main(String[] args) {
-        int testMax = TestMax.maxInt(20, 70, 60);
-        System.out.println("Maximum value in integer is : " + testMax);
+        int intValue = TestMax.maxInt(20, 70, 60);
+        System.out.println("Maximum value in integer is : " + intValue);
+
+        float floatValue = TestMax.maxFloat(10.5f, 20.01f, 30.9f);
+        System.out.println("Maximum value in float is : " + floatValue);
     }
 }
