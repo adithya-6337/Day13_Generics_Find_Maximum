dd9158665d640067d0e503a0a2fab1feaaa2bb67
adithya-6337 <veldandiadithya337@gmail.com>
2022-10-19 12:00:00 +0530

    Added : using maximum test cases

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,18 +1,14 @@
 package com.bridgelabz;
 
 public class Main {
-
     public static void main(String[] args) {
-        TestMax<Integer> integerTestMax = new TestMax<>();
-        int intValue = integerTestMax.maxValue(2, 5, 7);
-        System.out.println("Maximum value in integer : " + intValue);
+        Integer integerValue = MaximumTest.maxValue(10, 20, 30);
+        System.out.println("Maximum value in integer is :" + integerValue);
 
-        TestMax<Float> floatTestMax = new TestMax<>();
-        float floatValue = floatTestMax.maxValue(2.3f, 4.5f, 9.99f);
-        System.out.println("Maximum value in float : " + floatValue);
+        Float floatValue = MaximumTest.maxValue(12.3f, 14.5f, 19.99f);
+        System.out.println("Maximum value in float is : " + floatValue);
 
-        TestMax<String> stringTestMax = new TestMax<>();
-        String stringValue = stringTestMax.maxValue("Apple", "Peach", "Banana");
-        System.out.println("Maximum value in string : " + stringValue);
+        String stringValue = MaximumTest.maxValue("Apple", "Peach", "Banana");
+        System.out.println("Maximum value in string is : " + stringValue);
     }
 }
